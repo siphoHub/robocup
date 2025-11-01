35c8af9fcd0839fcdab1fad57af8db6dd6c8df87
siphoHub <2430285@students.wits.ac.za>
2025-11-01 12:14:22 +0200

    fix start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@ export OMP_NUM_THREADS=1
 host=${1:-localhost}
 port=${2:-3100}
 
+# Pre-build C++ modules once before launching multiple players in parallel.
+python3 - <<'PY'
+from scripts.commons.Script import Script
+Script.build_cpp_modules()
+PY
+
 for i in {1..5}; do
   python3 ./Run_Player.py -i $host -p $port -u $i -t sipho -P 0 -D 0 &
   sleep 0.9
